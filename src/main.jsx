775f77506d5522e6de3318483ed550c4ab243080
Samuteg <samuneveslopes@gmail.com>
2026-01-18 12:13:03 -0300

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
       <SpeedInsights />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
